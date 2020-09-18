bc2a4856729b76e8c2d8887e40916a9a47d7e19f
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-09-18 13:41:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2_Numbers_In_C
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            //addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            //subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            //multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            //division
+            c = a / b;
+            Console.WriteLine(c);
+
+            c = a + b - 12 * 17;
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+
+       static void Main(string[]) args)
+       {
+            WorkingWithIntigers();
+       }
+    }
+}
